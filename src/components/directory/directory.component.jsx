79ecf05bfd3812e99f117cdf3aea6873e4543885
11VladC11 <vladw239@gmail.com>
2022-06-25 07:18:52 +0300

    added assests in google photos

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,32 +1,33 @@
 import DirectoryItem from "../directory-item/directory-item.component"
 import { DirectoryContainer } from "./directory.styles"
-import vikingImg from '../../assets/Small-designs/viking.jpg';
-import firstWallImg from '../../assets/Wall-designs/first-wall.jpg';
-import accessoriesImg from '../../assets/Accessories/plastic-pvc.jpg'; 
-import comingSoonImg from '../../assets/Sketches/sketch-home.jpg';
+// import vikingImg from '../../assets/Small-designs/viking.jpg';
+// import firstWallImg from '../../assets/Wall-designs/first-wall.jpg';
+// import accessoriesImg from '../../assets/Accessories/plastic-pvc.jpg'; 
+// import comingSoonImg from '../../assets/Sketches/sketch-home.jpg';
+import {scketches, viking, workspaceWall, plasticPvc } from '../../shop-data';
 const categories = [
 	{
 		id:1,
 		title:'small designs',
-		imageUrl: vikingImg,
+		imageUrl: viking,
 		route:'shop/small-designs'
 	},
 	{
 		id:2,
 		title:'wall designs',
-		imageUrl:firstWallImg,
+		imageUrl:workspaceWall,
 		route:'/shop/wall-designs'
 	},
 	{
 		id:3,
 		title:'accessories',
-		imageUrl:accessoriesImg,
+		imageUrl: plasticPvc,
 		route:'/shop/accessories',
 	},
 	{
 		id:4,
 		title:'coming soon',
-		imageUrl:comingSoonImg,
+		imageUrl:scketches,
 	}
 ]
 
--- src/shop-data.js
+++ src/shop-data.js
@@ -1,19 +1,40 @@
-import guitar from './assets/Small-designs/1gr.jpg';
-import hflower from './assets/Small-designs/1hr.jpg';
-import thearts from './assets/Small-designs/2hr.jpg';
-import dragon from './assets/Small-designs/dragon.jpg';
-import phoenixOne from './assets/Small-designs/phoenix.jpg';
-import viking from './assets/Small-designs/viking.jpg';
-import eagle from './assets/Small-designs/vultur.jpg';
-import wolf from './assets/Small-designs/wr.jpg';
-import phoenixTwo from './assets/Small-designs/phoenix.v2.jpg';
-import workspaceWall from './assets/Wall-designs/first-wall.jpg';
-import bullBear from './assets/Wall-designs/biciara3.jpg';
-import worldMap from './assets/Wall-designs/world-map.jpg';
-import dreamerWall from './assets/Wall-designs/second-wall.jpg';
-import adhesiveTape from './assets/Accessories/bandadublsimp.webp';
-import siliconTape from './assets//Accessories/bandadublasilic.jpg';
-import plasticPvc from './assets/Accessories/plastic-pvc.jpg';
+//small-designs
+const guitar = 'https://lh3.googleusercontent.com/451pEOKgFCbwp6OshjcOiTGuFPtk4DU7vW9ffm2hIj7cSMHZVNdMb6ovIk-wiPI89Kxj5YYsM_3BiGib3MXDx18YGxDLBmihp3oXuhTvlBC5Q8KxPAXAF2jirti_m154-jVZMkK0=w2400';
+
+const hflower = 'https://lh3.googleusercontent.com/Y0CzBMgqRgVIYgLc3A7n1UvCRcKdCAbCw6IfEHnAe-8ONpMX5H3rWh356MEub6NLQsvm6MkJxVtS1MG_C6db6iwHwQiRGXNWWFCuzg-K0yMgsbk_geKPev76Env2OzBE7GbZe8Ci=w2400';
+
+const thearts = 'https://lh3.googleusercontent.com/C85ypVwICCAGBd0W7jpZIxx3Ats2_HGAHi3WP9ynlc145Hsze5cT4gz5lhH7J5smrX3LVwRhPwIKxykuImg95kTtbXFgHanroVhjlVbau9My94aYI5bBOLOhaIikY_DunGWbTRtG=w2400';
+
+const dragon = 'https://lh3.googleusercontent.com/sLKLKs1lrje-lwRso_60OB5Ff0pxcpCA88PUbiddm_abfFYUxWi82Q8Ez4nX1fq8q0nTk37FX-y5gHYdzfRSFiSpwsQkZxBq5BOzVfbv5Z86mU45OW8mKJwRRnJURgmZHnvHw3sv=w2400';
+
+const phoenixOne = 'https://lh3.googleusercontent.com/Hi3na9qbjf8V_c_zvCK_Mw5UvYKllmJBQgZNnqlc2NUsEHa7bI33yIFRo2HOqtGmxnvejx3pu09WhHCmMCCDhjSQ8rMAndvdUKhIW1BEBAR1PObGSPzmzscGHfqCTNq0fOxoANP0=w2400';
+
+export const viking = 'https://lh3.googleusercontent.com/nSPJ_sMhssl2RFBxZPqeFRXf0p8nqETp92L7oomFE3uqXRIdPmKdLMK-KfBMYPCPfLQ8O_Sb6hBN5bD3sOT9N0pONwUdjsVcC1YVRropwqCMp-8CklU6omgFM_6JPQn9AOHnpf_K=w2400';
+
+const eagle = 'https://lh3.googleusercontent.com/l5PyWdtTW-QQDD4rftyxfeGVhpdZ76GWx7ItVEYfOKFLi__CmDTeG2KfmnWGqYQ-2aiFC46dblhCX13TWVIFeqU1Wb8wPr6rHzkCoTDKDt72b3Lkjs9l58g9BgnvYWxwxH202Pzm=w2400';
+
+const wolf = 'https://lh3.googleusercontent.com/H_QkTrXFql99g7P3CwQaHDPSj3jXVOJUMq9sGr3bvpTOGb9r2LIo2Ie4xlLjhQR9Dtz2U1BJSeAKzf_ATzyWKcnGYqM4fF5vpmhKnB3PdFtBknbhI-pT4qSAp2XZz2oYYme2EKOf=w2400';
+
+const phoenixTwo = 'https://lh3.googleusercontent.com/DeDRyd3gcY6A1DCJzGoSU1jV_sRaNUz4q1Ak2QjE4uCjqd_SGwDJAjkYGdAvtfXTcQnshVLAgSIkPTCQSMWfyNUpGGhXwxiiVXIMueAwGq_mO5ytaAhSImk0WAOlnqRDyGt6cFyQ=w2400';
+
+//wall-designs
+export const workspaceWall = 'https://lh3.googleusercontent.com/w6ztan5iw7Qb9illMIH5mMxBOWJFLonIn0TMCXNxnfX-sL2386bxyQekdmuz97-HDWSrq2-nmqjgvMEwvLy9LsFhRzOXyJwQmi0DOS8yCMW1EyOrFvC8Z5H21KhTvFLtuoqCe3_S=w2400';
+
+const bullBear = 'https://lh3.googleusercontent.com/a_nDwUBQyQvYgCIb0_0LtPj_NnN43h4dJgjdAed7ynFQfltry3atYBbIEESBZHsIb8BRFoibhIywhJMDFOGeHhy6Nv0k0GmEmGqR9dnWcQVwvKJNGWOLpKp_h8iCFohgsgsMjO-U=w2400';
+
+const worldMap = 'https://lh3.googleusercontent.com/rrw9cI7-1Z5xUNHuCc7c7LrE5Lv-8yGsoLU3Tm3syyp8rP2Rmo-DxCnMY0Eh5WNaC1sYn8AOwmCyqlJho_2alRQkyrX75ANMioVwJmLZBTuonZGJkVB_DiUXA0ANvEnaoaKJesHK=w2400';
+
+const dreamerWall = 'https://lh3.googleusercontent.com/u8rh372oiXS9cl_ceJLZQkyCfiFETW3HDpbWDVM_14fjToqftZ5VqejFyjgtttMILHeVxObPRVnp4tgQSF1c_SQLPTSwClK9qlhbtDIQ3325mmJ0CvfKNzctzwJaz-Lc7Ia5nUW4=w2400';
+
+//accessories
+const adhesiveTape = 'https://lh3.googleusercontent.com/GCqq4tJUpWKQZDL3pOrKx2GEUsVrua9cb97J-sm-GyMXmEYFeX9W550WVwS1kt83Q8__2GlN8Mmq-XLZ58FQt3Z0JHmlyKjN8vp44wxt2fmwCd1L-rKWFCLoQzXVAJwwXMieWREE=w2400';
+
+const siliconTape ='https://lh3.googleusercontent.com/pFmIer0ftWDeDzAka65Y8fCsZmYQ5r4P3bGeABl1wXd_VJpByljY4cTyVep1L8RPoAkrAy4wIQp6UKP8ktR45c83gn59cmXatZHUHph2tIzdbhoZHBFi95IzifZUKqysiEtfqfnY=w2400';
+
+export const plasticPvc = 'https://lh3.googleusercontent.com/LXagw68dSWKY8QuHCPRXQASZPaVGTuTwovz5ergj3qcHhBQh2RrycC3ywF3Yjw-hEDbq1K-nE8j2kfKZMxaeEZfN3i4Ykaa5G3RD1qxWmcLwL5azvGmyDtQKnvJdsULcxyIbsKT6=w2400';
+
+export const scketches ='https://lh3.googleusercontent.com/BuRDleOG1EWSsOLTVbt61iL2br9gJfz6njiqq-ksYBa7bgGT8AV_Czp7lW_X-L8hxkkLK79BUvKL0fvQw-AAVmp0tB8tdS45PpslH7J4hw9uPUtclTcmxPwceu_kw6J3v7vJarp9=w2400'
+
 const SHOP_DATA =[
 	{
 		title:'Small-designs',
